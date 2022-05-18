36ac60ae7237a8ac395686cc46edb3f76dac9dcd
killyvera <david.esmeralda@gmail.com>
2022-05-18 00:59:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SWOTCard from './components/SWOTCard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SWOTCard />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SWOTCard.js
@@ -0,0 +1,7 @@
+export default function SWOTCard(){
+    return(
+        <div>
+        <h1>SWOT Card</h1>
+        </div>
+    );
+};
