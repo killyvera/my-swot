9ee57418eba68c44c8794b81cc31b090d31ce79e
killyvera <david.esmeralda@gmail.com>
2022-05-18 04:40:13 -0500

    some advance styling swot card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import SwotCard from './components/SwotCard'
 import './App.css';
 
-const cardContent ={counter:'00', firstContent: 'Tech Industry'}
+const cardContent1 ={
+  counter:'01',
+   firstContent: 'Tech Industry',
+    otherContents:[
+      'Communicative',
+      'Creative',
+      'Good Mate',
+      'Innovative'
+    ],
+  };
+
+  const cardContent2 ={
+    counter:'02',
+     firstContent: 'Tech Industry',
+      otherContents:[
+        'Communicative',
+        'Creative',
+        'Good Mate',
+        'Innovative'
+      ],
+    };
 
 function App() {
   return (
     <div className="App">
       <SwotCard
-      counter={cardContent.counter}
-      firstContent={cardContent.firstContent}
-      
+      counter={cardContent1.counter}
+      firstContent={cardContent1.firstContent}
+      otherContents={cardContent1.otherContents}
+      />
+
+    <SwotCard
+      counter={cardContent2.counter}
+      firstContent={cardContent2.firstContent}
+      otherContents={cardContent2.otherContents}
       />
 
     </div>
--- src/components/SWOTCard.js
+++ src/components/SWOTCard.js
@@ -8,6 +8,9 @@ export default function SwotCard(props){
             <ContentWrapper>
                 <CounterCard>{props.counter}</CounterCard>
                 <FirstContent>{props.firstContent}</FirstContent>
+                <OtherContents>
+                    {props.otherContents.map((content, index)=><p key={index} >{content}</p>)}
+                </OtherContents>
             </ContentWrapper>
         </CardWrapper>
 
@@ -15,66 +18,73 @@ export default function SwotCard(props){
 };
 
 let CardWrapper = styled.div`
-width: 200px;
-height: 360px;
-background: linear-gradient(178.19deg, #FFBB00 45.24%, #FF8F00 103.56%);
-box-shadow: -17px 25px 15px rgba(0, 0, 0, 0.25);
-border-radius: 100px;
-display: flex;
-flex-direction: column;
-align-items: center;
+    position:relative;
+    width: 200px;
+    height: 360px;
+    background: linear-gradient(178.19deg, #FFBB00 45.24%, #FF8F00 103.56%);
+    box-shadow: -17px 25px 15px rgba(0, 0, 0, 0.25);
+    border-radius: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 let Circle = styled.div`
-width: 175px;
-height: 175px;
-background: #434343;
-box-shadow: -12px 18px 17px rgba(0, 0, 0, 0.33), inset 49px -23px 72px #0F0F0F;
-border-radius: 100px;
-margin-top: 12px
+    width: 175px;
+    height: 175px;
+    background: #434343;
+    box-shadow: -12px 18px 17px rgba(0, 0, 0, 0.33), inset 49px -23px 72px #0F0F0F;
+    border-radius: 100px;
+    margin-top: 12px;
 `
 let ContentWrapper = styled.div`
-background-color: gray;
-position: absolute;
-width: 180px;
-height: 105px;
-left: 10px;
-top: 200px;
+    position: absolute;
+    width: 180px;
+    height: 105px;
+    left: 10px;
+    top: 200px;
 `
 let CounterCard = styled.h1`
-
-background-color: white;
-
-position: absolute;
-width: 62px;
-height: 70px;
-left: 0px;
-bottom: -3px;
-
-font-family: 'Impact';
-font-style: normal;
-font-weight: 400;
-font-size: 57px;
-line-height: 70px;
-
-display: flex;
-align-items: center;
-color: #1F1F1F;
+    position: absolute;
+    width: 62px;
+    height: 70px;
+    left: 0px;
+    bottom: -7px;
+    font-family: 'Impact';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 58px;
+    line-height: 70px;
+    display: flex;
+    align-items: center;
+    color: #1F1F1F;
 `
+
 const FirstContent= styled.h1`
-
-background-color: red;
-
-position: absolute;
-width: 119px;
-height: 14px;
-left: 62px;
-bottom: 70px;
-
-font-family: 'Kodchasan';
-font-style: normal;
-font-weight: 700;
-font-size: 13px;
-line-height: 103.16%;
-text-transform: uppercase;
-color: #1F1F1F;
+    position: absolute;
+    width: 119px;
+    height: 14px;
+    left: 62px;
+    bottom: 70px;
+    font-family: 'Kodchasan';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 13px;
+    line-height: 103.16%;
+    text-transform: uppercase;
+    text-align: left;
+    color: #1F1F1F;
+`
+const OtherContents = styled.div`
+    text-align: left;
+    position: absolute;
+    width: 119px;
+    height: 37px;
+    left: 62px;
+    top: 27px;
+    font-family: 'Kodchasan';
+    font-style: normal;
+    font-size: 8px;
+    line-height: 0%;
+    text-transform: uppercase;
+    color: #1F1F1F;
 `
